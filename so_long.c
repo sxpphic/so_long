0ac0bcef5129ad3f1e19b1c609dcfea50a87aa7d
vipereir <zzvtrx@gmail.com>
2022-09-26 16:17:50 -0300

    printando sprinte na tela

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: vipereir <vipereir@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 15:14:39 by vipereir          #+#    #+#             */
-/*   Updated: 2022/09/26 11:55:04 by vipereir         ###   ########.fr       */
+/*   Updated: 2022/09/26 16:13:55 by vipereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,41 +20,24 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
-void	print_square(t_data *img, int x, int y, int size, int color)
-{
-	int	i;
-
-	(void)x;
-	i = 0;
-	while (i <= size)
-		my_mlx_pixel_put(img, (x + i++), y, color);
-	i = 0;
-	while (i <= size)
-		my_mlx_pixel_put(img, x + size, (y + i++), color);
-	i = size;
-	while (i >= 0)
-		my_mlx_pixel_put(img, (x + i--), y + size, color);
-	i = size;
-	while (i >= 0)
-		my_mlx_pixel_put(img, x, (y + i--), color);
-}
-
 int	main(void)
 {
-	int	i;
+
 	void	*mlx;
 	void	*mlx_win;
 	t_data	img;
+	char	*relative_path = "./assets/ground.xpm";
+	int		img_width;
+	int		img_height;
 
-	i = 0;
+
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 720, 720, "Hello world!");
-	img.img = mlx_new_image(mlx, 720, 720);
+	mlx_win = mlx_new_window(mlx, 500, 500, "Hello world!");
+	img.img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	print_square(&img, 5, 5, 20, 0x00FF00);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-
 	mlx_loop(mlx);
+
 }
 
 
